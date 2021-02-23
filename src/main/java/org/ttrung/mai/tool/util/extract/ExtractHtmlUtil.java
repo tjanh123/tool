11d7c49259e77f6cd5b84b6d416e030d344112e0
ttrung.mai <ttrung.mai@gmail.com>
2021-02-24 00:10:33 +0100

    add check empty when extracting img from description

--- src/main/java/org/ttrung/mai/tool/util/extract/ExtractHtmlUtil.java
+++ src/main/java/org/ttrung/mai/tool/util/extract/ExtractHtmlUtil.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * extract util especially for html 
  * @author ttrung.mai
@@ -19,6 +21,9 @@ public class ExtractHtmlUtil {
 	 * @return
 	 */
 	public static List<String> extractSourceImages(String text) {
+		if(StringUtils.isBlank(text)) {
+			return new ArrayList<>();
+		}
 		Pattern pattern = Pattern.compile("(?<=<img src=\\\")[^\\\"]*", Pattern.DOTALL);
 		final List<String> tagValues = new ArrayList<String>();
 		final Matcher matcher = pattern.matcher(text);
