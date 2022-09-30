f56be2f372cf1d48320e7580a23ff87706be87b6
ttrung.mai <ttrung.mai@gmail.com>
2022-09-30 22:25:37 +0200

    add resource notfound exception

---
+++ src/main/java/org/ttrung/mai/tool/exception/authentication/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package org.ttrung.mai.tool.exception.authentication;
+
+import org.ttrung.mai.tool.exception.GeneralException;
+
+public class ResourceNotFoundException extends GeneralException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Class<?> clazz, Object id) {
+        super("0003", 404, "Resource " + clazz + " with ID " + id + " is not found");
+    }
+
+}
