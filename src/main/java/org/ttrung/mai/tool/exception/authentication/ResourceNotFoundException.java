1797b831097bdb1902a6685bedcf89914c332851
ttrung.mai <ttrung.mai@gmail.com>
2024-01-09 22:15:39 +0100

    update error code 404

--- src/main/java/org/ttrung/mai/tool/exception/authentication/ResourceNotFoundException.java
+++ src/main/java/org/ttrung/mai/tool/exception/authentication/ResourceNotFoundException.java
@@ -10,11 +10,11 @@ public class ResourceNotFoundException extends GeneralException {
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Class<?> clazz, Object id) {
-        super("0003", 404, "Resource " + clazz + " with ID " + id + " is not found");
+        super("0004", 404, "Resource " + clazz + " with ID " + id + " is not found");
     }
 
     public ResourceNotFoundException(String domain, Object id) {
-        super("0003", 404, "Resource " + domain + " with ID " + id + " is not found");
+        super("0004", 404, "Resource " + domain + " with ID " + id + " is not found");
     }
 
 }
